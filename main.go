4576fcbcdc9a0bd2ab11c8b14b4146f52e0bfdf3
Wisarut Chaloemanukulwattana <wisarutkim012@gmail.com>
2022-12-03 15:44:07 +0700

    add grace fully shutdow

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/joho/godotenv"
 
@@ -45,5 +51,34 @@ func main() {
 	proteced.POST("/todos", handler.NewTask)
 	r.GET("/todos/:id", handler.GetTodo)
 	r.GET("/todos", handler.GetTodoList)
-	r.Run()
+
+	// create notify context for recived signal SIGINT or SIGTERM
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	s := &http.Server{
+		Addr:           ":" + os.Getenv("PORT"),
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen : %s\n", err)
+		}
+	}()
+
+	//when signal sigint or sigterm input it'll stop serivce
+	<-ctx.Done()
+	stop()
+	fmt.Println("Shutting down gracefully, press Ctrl+C again to force")
+
+	timeoutContext, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(timeoutContext); err != nil {
+		fmt.Println(err)
+	}
 }
